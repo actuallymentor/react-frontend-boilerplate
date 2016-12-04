9de649803e353e5d6e8e48edf3f21b36ca6aa39f
Mentor <mentor@palokaj.co>
2016-12-04 15:06:48 +0100

    Implemented enzyme based testing

--- test/header.js
+++ test/header.js
@@ -12,12 +12,12 @@ import { Panel, Header } from '../src/state/head'
 describe( '<Panel />', f => {
 
 	// Render a Panel component
-	const wrapper = mount( <Panel id = 'menu' /> )
+	const panel = mount( <Panel id = 'menu' /> )
 
 	it( 'Sets id prop', done => {
 
 		// Check if a regular render works
-		expect( wrapper.props( ).id ).to.equal( 'menu' )
+		expect( panel.props( ).id ).to.equal( 'menu' )
 		done( )
 
 	} )
@@ -25,8 +25,8 @@ describe( '<Panel />', f => {
 	it( 'Can change id prop', done => {
 
 		// Check if we can change it
-		wrapper.setProps( {id: "changed"} )
-		expect( wrapper.props( ).id ).to.equal( 'changed' )
+		panel.setProps( {id: "changed"} )
+		expect( panel.props( ).id ).to.equal( 'changed' )
 		done( )
 
 	} )
@@ -34,9 +34,9 @@ describe( '<Panel />', f => {
 	it( 'Toggles visibility', done => {
 
 		// Check if panel is hidden by default
-		expect( wrapper.state( ).visible ).to.be.false
-		wrapper.find( '#menu-btn' ).simulate( 'click' )
-		expect( wrapper.state( ).visible ).to.be.true
+		expect( panel.state( ).visible ).to.be.false
+		panel.find( '#menu-btn' ).simulate( 'click' )
+		expect( panel.state( ).visible ).to.be.true
 		done( )
 
 	} )
@@ -44,8 +44,8 @@ describe( '<Panel />', f => {
 	it( 'Displays menu items', done => {
 
 		// Check if menu item objects are rendered
-		let stateItems = wrapper.state( ).menuLinks.length
-		let renderedItems = wrapper.find( '.menuitem' )
+		let stateItems = panel.state( ).menuLinks.length
+		let renderedItems = panel.find( '.menuitem' )
 		expect( renderedItems ).to.have.length( stateItems )
 		done( )
 
@@ -55,7 +55,7 @@ describe( '<Panel />', f => {
 
 describe( '<Header />', f => {
 
-	const wrapper = mount(
+	const panel = mount(
 			<Header
 				id 		 = "header"
 				title 	 = "Home"
@@ -67,7 +67,7 @@ describe( '<Header />', f => {
 	it( 'Sets header id', done => {
 
 		// Find the header element and check the ID
-		let header = wrapper.find( '#header' )
+		let header = panel.find( '#header' )
 		expect( header.prop( 'id' ) ).to.equal( 'header' )
 		expect( header )
 		done( )
@@ -77,24 +77,24 @@ describe( '<Header />', f => {
 	it( 'Shows & hides the logo, and shows brand name', done => {
 
 		// Find the logo and check visibility
-		let logo = wrapper.find( 'img#logo' )
+		let logo = panel.find( 'img#logo' )
 		expect( logo.prop( 'src' ) ).to.equal( 'logo.png' )
 		expect( logo.prop( 'class' ) ).to.equal( undefined )
 
 		// Remove the logo path and check for brand name
-		wrapper.setProps( { logo: undefined } )
+		panel.setProps( { logo: undefined } )
 		expect( logo.prop( 'className' ) ).to.equal( 'hide' )
 
 		// Shows brand name
-		let identity = wrapper.find( '#identity' ).text( )
+		let identity = panel.find( '#identity' ).text( )
 		expect( identity ).to.equal( 'Identity' )
 
 		done( )
 	} )
 
 	it( 'Sets the title and subtitle', done => {
-		let title = wrapper.find( '#title' ).text( )
-		let subtitle = wrapper.find( '#subtitle' ).text( )
+		let title = panel.find( '#title' ).text( )
+		let subtitle = panel.find( '#subtitle' ).text( )
 
 		expect( title ).to.equal( 'Home' )
 		expect( subtitle ).to.equal( 'Welcome stranger' )
