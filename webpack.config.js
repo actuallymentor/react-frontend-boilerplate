4e49643ecf3206cbca6639ecaf12d4cc209a75e1
Mentor <mentor@palokaj.co>
2017-04-25 14:49:36 +0200

    Update to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,109 @@
 let BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 let autoprefixer = require ( 'autoprefixer' )
 let webpack = require( 'webpack' )
+const bs = require( 'browser-sync' )
+const ip = require( 'ip' )
 
-const pluginarray = env => {
-  if ( env == 'production' ) {
-    return [
-    new webpack.DefinePlugin( {
-      'process.env': {
-        NODE_ENV: JSON.stringify( 'production' )
-      }
-    } ),
-    new webpack.optimize.UglifyJsPlugin( {
-      compress: {
-        warnings: false
-      }
-    })
+// const pluginarray = env => {
+//   if ( env == 'production' ) {
+//     return [
+//     new webpack.DefinePlugin( {
+//       'process.env': {
+//         NODE_ENV: JSON.stringify( 'production' )
+//       }
+//     } ),
+//     new webpack.optimize.UglifyJsPlugin( {
+//       compress: {
+//         warnings: false
+//       }
+//     })
+//     ]
+//   } else if ( env == 'development' ) {
+//     return [
+//     new BrowserSyncPlugin( {
+//       host: 'localhost',
+//       port: 3000,
+//       server: { baseDir: ['public'] },
+//       notify: {
+//         styles:  [
+//         "display: none",
+//         "padding: 15px",
+//         "font-family: sans-serif",
+//         "position: fixed",
+//         "font-size: 0.9em",
+//         "z-index: 9999",
+//         "bottom: 0px",
+//         "right: 0px",
+//         "border-bottom-left-radius: 5px",
+//         "background-color: #1B2032",
+//         "margin: 0",
+//         "color: white",
+//         "text-align: center"
+//         ]
+//       }
+//     } )
+//     ]
+//   } else {
+//     return []
+//   }
+// }
+
+// ///////////////////////////////
+// Plugins
+// ///////////////////////////////
+let thebs
+const servername = 'bsserver'
+const bsconfig = {
+  host: ip.address(),
+  open: true,
+  port: 3000,
+  server: { 
+    baseDir: [ 'public' ],
+    serveStaticOptions: {
+      extensions: ['html']
+    }
+  },
+  https: true,
+  // proxy: 'https://web.dev:3000',
+  notify: {
+    styles:  [
+    "display: none",
+    "padding: 15px",
+    "font-family: sans-serif",
+    "position: fixed",
+    "font-size: 0.9em",
+    "z-index: 9999",
+    "bottom: 0px",
+    "right: 0px",
+    "border-bottom-left-radius: 5px",
+    "background-color: #1B2032",
+    "margin: 0",
+    "color: white",
+    "text-align: center"
     ]
-  } else if ( env == 'development' ) {
-    return [
-    new BrowserSyncPlugin( {
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['public'] },
-      notify: {
-        styles:  [
-        "display: none",
-        "padding: 15px",
-        "font-family: sans-serif",
-        "position: fixed",
-        "font-size: 0.9em",
-        "z-index: 9999",
-        "bottom: 0px",
-        "right: 0px",
-        "border-bottom-left-radius: 5px",
-        "background-color: #1B2032",
-        "margin: 0",
-        "color: white",
-        "text-align: center"
-        ]
-      }
-    } )
-    ]
-  } else {
-    return []
   }
 }
+const bsyncplugconfig = {
+  name: servername,
+  callback: f => { thebs = bs.get( servername ) }
+}
+
+// Get environment variables
+require('dotenv').config( `${__dirname}/.env` )
+// Remap process env
+const stringify_env = f => {
+  let environment = {}
+  Object.keys( process.env ).map( ( key, index ) => { return environment[ key ] = JSON.stringify( process.env[ key ] ) } )
+  return environment
+}
+
+const plugins = process.env.NODE_ENV == 'production' ?
+  [ new webpack.optimize.UglifyJsPlugin( { compress: { warnings: false }, sourceMap: true } ),
+    new webpack.DefinePlugin( { 'process.env': { NODE_ENV: JSON.stringify( 'production' ) } } ),
+    new webpack.DefinePlugin( stringify_env( ) ) ]
+  :
+  [ new BrowserSyncPlugin( bsconfig, bsyncplugconfig ),
+    new webpack.DefinePlugin( stringify_env( ) ) ]
 
 const maps = env => {
   if( env == 'production' ) {
@@ -57,29 +116,64 @@ const maps = env => {
 console.log( 'Environment is ' + process.env.NODE_ENV )
 console.log( 'Source maps are using ' + maps( process.env.NODE_ENV ) )
 
+// module.exports = {
+//   entry: __dirname + '/src/main.js',
+//   output: {
+//     filename: __dirname + '/public/js/app.js'
+//   },
+//   module: {
+//     loaders: [
+//     {
+//       test: /\.js$/,
+//       loader: 'babel-loader',
+//       query: {
+//         presets: ['es2015', 'react']
+//       }
+//     },
+//     {
+//       test: /\.scss$/,
+//       loaders: ["style", "css", "sass", "postcss"]
+//     }
+//     ]
+//   },
+//   devtool: maps( process.env.NODE_ENV ),
+//   postcss: [
+//   autoprefixer( { browsers: ['last 2 versions'] } )
+//   ],
+//   plugins: pluginarray( process.env.NODE_ENV )
+// }
+
 module.exports = {
-  entry: __dirname + '/src/main.js',
+  entry: `${__dirname}/src/main.js`,
   output: {
-    filename: __dirname + '/public/js/app.js'
+    filename: 'app.js',
+    path: `${__dirname}/public/js/`
   },
   module: {
-    loaders: [
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+          // options: { presets: [ 'react', 'es2015' ] }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: f => { return [ autoprefixer( { browsers: ['last 2 versions'] } ) ] }
+            }
+          },
+          "sass-loader" ]
       }
-    },
-    {
-      test: /\.scss$/,
-      loaders: ["style", "css", "sass", "postcss"]
-    }
     ]
   },
-  devtool: maps( process.env.NODE_ENV ),
-  postcss: [
-  autoprefixer( { browsers: ['last 2 versions'] } )
-  ],
-  plugins: pluginarray( process.env.NODE_ENV )
+  devtool: process.env.NODE_ENV == 'production' ?  'cheap-module-source-map' : 'eval',
+  plugins: plugins
 }
