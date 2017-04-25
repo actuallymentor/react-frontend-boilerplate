6403d0d8912f902256a37f5abb80deac4ddc1c40
Mentor <mentor@palokaj.co>
2017-04-25 14:50:33 +0200

    Remove legacy code

--- webpack.config.js
+++ webpack.config.js
@@ -4,50 +4,6 @@ let webpack = require( 'webpack' )
 const bs = require( 'browser-sync' )
 const ip = require( 'ip' )
 
-// const pluginarray = env => {
-//   if ( env == 'production' ) {
-//     return [
-//     new webpack.DefinePlugin( {
-//       'process.env': {
-//         NODE_ENV: JSON.stringify( 'production' )
-//       }
-//     } ),
-//     new webpack.optimize.UglifyJsPlugin( {
-//       compress: {
-//         warnings: false
-//       }
-//     })
-//     ]
-//   } else if ( env == 'development' ) {
-//     return [
-//     new BrowserSyncPlugin( {
-//       host: 'localhost',
-//       port: 3000,
-//       server: { baseDir: ['public'] },
-//       notify: {
-//         styles:  [
-//         "display: none",
-//         "padding: 15px",
-//         "font-family: sans-serif",
-//         "position: fixed",
-//         "font-size: 0.9em",
-//         "z-index: 9999",
-//         "bottom: 0px",
-//         "right: 0px",
-//         "border-bottom-left-radius: 5px",
-//         "background-color: #1B2032",
-//         "margin: 0",
-//         "color: white",
-//         "text-align: center"
-//         ]
-//       }
-//     } )
-//     ]
-//   } else {
-//     return []
-//   }
-// }
-
 // ///////////////////////////////
 // Plugins
 // ///////////////////////////////
@@ -116,32 +72,6 @@ const maps = env => {
 console.log( 'Environment is ' + process.env.NODE_ENV )
 console.log( 'Source maps are using ' + maps( process.env.NODE_ENV ) )
 
-// module.exports = {
-//   entry: __dirname + '/src/main.js',
-//   output: {
-//     filename: __dirname + '/public/js/app.js'
-//   },
-//   module: {
-//     loaders: [
-//     {
-//       test: /\.js$/,
-//       loader: 'babel-loader',
-//       query: {
-//         presets: ['es2015', 'react']
-//       }
-//     },
-//     {
-//       test: /\.scss$/,
-//       loaders: ["style", "css", "sass", "postcss"]
-//     }
-//     ]
-//   },
-//   devtool: maps( process.env.NODE_ENV ),
-//   postcss: [
-//   autoprefixer( { browsers: ['last 2 versions'] } )
-//   ],
-//   plugins: pluginarray( process.env.NODE_ENV )
-// }
 
 module.exports = {
   entry: `${__dirname}/src/main.js`,
@@ -156,7 +86,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
-          // options: { presets: [ 'react', 'es2015' ] }
         }
       },
       {
